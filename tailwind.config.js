14887eea4698a709e11b898ae5f235c45c17decc
Kevin Chavez <kevinedi52@gmail.com>
2025-01-23 11:45:10 -0600

    added: added talwind in my project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}" // Incluye todos los archivos .html y .ts dentro de src/
+  ],
+  theme: {
+    extend: {}, // Personaliza aquí tu configuración
+  },
+  plugins: [],
+};
