d6d6b0aae00ba3296a17d70f8a0990d74b82d6f5
Kevin Chavez <kevinedi52@gmail.com>
2025-01-28 13:32:18 -0600

    added: added data to API

--- src/app/features/anime/pages/home/home.component.ts
+++ src/app/features/anime/pages/home/home.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AnimesService } from '../../../../core/services/animes.service';
+import { AnimeSlider } from '../../models/anime-slider';
+import { PosterCardComponent } from '../../../../shared/components/poster-card/poster-card.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [PosterCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent {
+  private animeService = inject(AnimesService);
+  movieTabList = ['lasted anime', 'Anime upgraded'];
+  animeList: Array<AnimeSlider> = [];
+  selectedAnimeTab = 0;
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.getSliderAnimes('animes');
+    }, 0);
+  }
+
+  private async getSliderAnimes(type: string) {
+    try {
+      const response = await this.animeService.getSliderAnimes(type) as { data: { posts: AnimeSlider[] } };
+      this.animeList = response.data.posts;
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  onChangeTab(tabIndex: number) {
+    this.selectedAnimeTab = tabIndex;
+    this.getSliderAnimes(this.movieTabList[tabIndex]);
+  }
 }
