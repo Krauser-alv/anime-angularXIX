7e61c64a354b90387429751ebf35b10ed7064caa
Kevin Chavez <kevinedi52@gmail.com>
2025-01-28 13:55:41 -0600

    changed: changed the poster-card component

--- src/app/shared/components/poster-card/poster-card.component.ts
+++ src/app/shared/components/poster-card/poster-card.component.ts
@@ -6,19 +6,19 @@ import { environment } from '../../../../environments/environments';
 @Component({
   selector: 'app-poster-card',
   imports: [
-    RouterLink,
     DatePipe
   ],
   templateUrl: './poster-card.component.html',
   styleUrl: './poster-card.component.css'
 })
+
 export class PosterCardComponent {
   @Input() model!: any;
-  @Input() isMovie!: boolean;
   posterUrl!: string;
+
   private readonly apiUrl = environment.apiUrl + 'wp-content/uploads';
 
-  imageapiUrl(uuid: string): string {
+  imageApiUrl(uuid: string): string {
     return this.apiUrl + uuid;
   }
 }
