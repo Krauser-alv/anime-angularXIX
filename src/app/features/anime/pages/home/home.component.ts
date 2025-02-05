4569b46ed9b972295daedc938f52ceefc3db5b3b
Kevin Chavez <kevinedi52@gmail.com>
2025-02-05 15:56:56 -0600

    feat: implement Swiper carousel for anime recommendations with responsive configuration

--- src/app/features/anime/pages/home/home.component.ts
+++ src/app/features/anime/pages/home/home.component.ts
@@ -3,18 +3,41 @@ import { AnimesService } from '../../../../core/services/animes.service';
 import { AnimeSlider } from '../../models/anime-slider';
 import { PosterCardComponent } from '../../../../shared/components/poster-card/poster-card.component';
 import { CommonModule } from '@angular/common';
+import { SwiperDirective } from '../../../../shared/directives/swiper.directive';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-home',
-  imports: [PosterCardComponent, CommonModule],
+  imports: [PosterCardComponent, CommonModule, SwiperDirective],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeComponent {
+export class HomeComponent{
+  config: SwiperOptions = {
+    watchSlidesProgress: true,
+    grabCursor: true,
+    effect: 'coverflow', 
+    centeredSlides: false,
+    initialSlide: 2,
+    coverflowEffect: {
+      rotate: 12,
+      stretch: 0,
+      depth: 40,
+      modifier: 1,
+      slideShadows: false,
+    },
+    breakpoints: {
+      1440: {slidesPerView:9, spaceBetween: 25, slidesOffsetBefore: 0, slidesOffsetAfter: 0},
+      992: {slidesPerView: 6, spaceBetween: 25, slidesOffsetBefore: 0, slidesOffsetAfter: 0},
+      768: {slidesPerView: 4, spaceBetween: 25, slidesOffsetBefore: 0, slidesOffsetAfter: 0}, 
+      576: {slidesPerView: 2, spaceBetween: 25, slidesOffsetBefore: 0, slidesOffsetAfter: 0},
+      320: {slidesPerView: 2, spaceBetween: 25, slidesOffsetBefore: 0, slidesOffsetAfter: 0},
+    }
+  };
+
   public isLoading = signal(false);
   private animeService = inject(AnimesService);
-  movieTabList = ['lasted anime', 'Anime upgraded'];
   animeList: Array<AnimeSlider> = [];
   selectedAnimeTab = 0;
 
