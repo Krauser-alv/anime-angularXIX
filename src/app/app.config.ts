e9ce653aafd93fffdfbc8e8c5a3b88a34446df02
Kevin Chavez <kevinedi52@gmail.com>
2025-02-05 13:10:58 -0600

    config: setup app configuration with locale, animations, HTTP interceptor, and Swiper

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { register } from 'swiper/element/bundle';
+import { registerLocaleData } from '@angular/common';
+import localeEsMx from '@angular/common/locales/es-MX';
+import { progressBarInterceptor } from './shared/interceptors/progress-bar.interceptor';
+
+registerLocaleData(localeEsMx);
+
+register();
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
+    { provide: LOCALE_ID, useValue: 'es-MX' }, 
+    provideAnimationsAsync(), 
+    provideHttpClient(withInterceptors([progressBarInterceptor]))
+  ]
 };
