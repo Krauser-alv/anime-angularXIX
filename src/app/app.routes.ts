630af9d789f269f463bc713c423694c22ac7e1d8
Kevin Chavez <kevinedi52@gmail.com>
2025-02-13 13:42:31 -0600

    feat: add 404 not found page with animated alien and UFO theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: '', loadComponent: () => import('./features/anime/pages/home/home.component').then(m => m.HomeComponent)},
+    {path: '404', loadComponent: () => import('./shared/components/not-found/not-found.component').then(m => m.NotFoundComponent)},
+    {path: '**', redirectTo: '404'}
 ];
